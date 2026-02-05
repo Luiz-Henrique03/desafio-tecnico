3bcd1088cd0a8599746e12efecfd59f9532c8ebf
Luiz-Henrique03 <cumiranga@gmail.com>
2026-02-04 21:26:43 -0300

    Adicionado back e front dentro do docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,18 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true, 
     proxy: {
-      '/auth': 'http://localhost:8080',
-      '/clients': 'http://localhost:8080'
+      '/auth': {
+        target: 'http://avaliacao-backend:8080', 
+        changeOrigin: true,
+        secure: false,
+      },
+      '/clients': {
+        target: 'http://avaliacao-backend:8080', 
+        changeOrigin: true,
+        secure: false,
+      }
     }
   }
 })
